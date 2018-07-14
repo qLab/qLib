227091378c364e3e35eee4eea321b4648fc30d93
Imre Tuske <imre.tuske@gmail.com>
2018-07-14 13:01:44 +1200

    added otl builder/extractor tool script

---
+++ OTL.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+#	Quick-and-dirty utility script for extract Houdini .otl files to ascii and vice versa.
+#
+#
+
+
+# TODO: check if parameter 1 exists
+
+
+echo "otl.sh: \"$1\""
+
+pushd .
+
+
+case "$1" in
+
+	clean_built|cb|clean)
+		echo "*** CLEAN: DELETING BUILT OTLS ***"
+		find . -name "*.otl" -exec rm -rf {} \;
+	;;
+	#;& # (fallthru)
+
+
+
+	build|b)
+		echo "*** BUILDING OTLS ***"
+		find . -name "*_OTL" | awk '{ d=$0; sub(/_OTL$/, ".otl", d); print "hotl -C " $0 " " d }' | bash
+		find . -name "*.bkp*" -exec rm {} \; # delete backups created by otl building
+	;;
+
+
+
+	clean_extracted|cx)
+		echo "*** DELETING EXTRACTED OTLS ***"
+		find . -name "*_OTL" -exec rm -rf {} \;
+	;;
+	#;& # (fallthru)
+
+
+
+	extract|x)
+		echo "*** EXTRACTING OTLS ***"
+		find . -name "*.otl" | awk '{ d=$0; sub(/\.otl$/, "_OTL", d); print "hotl -X " d " " $0 }' | bash
+	;;
+
+
+
+	*)
+		echo "unknown command: \"$1\""
+	;;
+
+esac
+
+popd
+
+echo "done."
+
